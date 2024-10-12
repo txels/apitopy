c98d7aedd83017ad06db7f75e69e58dbe4772529
Carles Barrobés <carles@barrobes.com>
2024-10-12 21:45:56 +0200

    Use twine to release

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
-# # coding: utf-8
+# coding: utf-8
 
 from setuptools import setup
 
 setup(
-    name='apitopy',
-    description='Pythonic access to HTTP APIs',
-    long_description='Access arbitrary HTTP APIs as if they were python objects',
-    version='0.4.2',
-    author='Carles Barrobés',
-    author_email='carles@barrobes.com',
-    url='https://github.com/txels/apitopy',
-    py_modules=['apitopy'],
-    install_requires=['requests', 'supermutes'],
+    name="apitopy",
+    description="Pythonic access to HTTP APIs",
+    long_description="Access arbitrary HTTP APIs as if they were python objects",
+    long_description_content_type="text/markdown",
+    version="0.4.2",
+    author="Carles Barrobés",
+    author_email="carles@barrobes.com",
+    url="https://github.com/txels/apitopy",
+    py_modules=["apitopy"],
+    install_requires=["requests", "supermutes"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ],
 )
